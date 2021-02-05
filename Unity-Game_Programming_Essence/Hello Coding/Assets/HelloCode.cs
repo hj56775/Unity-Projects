267df5ca0fd7dfa05435f481ba30598e178600fa
hj56775 <68840193+hj56775@users.noreply.github.com>
2021-02-05 18:19:44 +0900

    c# 문법 학습

---
+++ Unity-Game_Programming_Essence/Hello Coding/Assets/HelloCode.cs
@@ -0,0 +1,91 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HelloCode : MonoBehaviour
+{
+    void Start()
+    {
+        //Hello World!를 콘솔에 출력
+        Debug.Log("Hello World!");
+        //캐릭터의 프로필을 변수로 만들기
+        string chracterName = "라라";
+        char bloodType = 'A';
+        int age = 17;
+        float height = 168.3f;
+        bool isFemale = true;
+
+        //생성한 변수를 콘솔에 출력
+        Debug.Log("캐릭터 이름 : " + chracterName);
+        Debug.Log("혈액형 : " + bloodType);
+        Debug.Log("나이 : " + age);
+        Debug.Log("키 : " + height);
+        Debug.Log("여성인가 : " + isFemale);
+
+        float distance = GetDistance(2, 2, 5, 6);
+        Debug.Log("(2,2)에서 (5,6)까지의 거리 : " + distance);
+
+        int love = 80;
+        if (love > 90)
+        {
+            //love가 90보다 큰 경우
+            Debug.Log("트루엔딩: 히로인과 결혼했다!");
+        }
+        else if (love > 70)
+        {
+            //love가 90보다 작거나 같고, 70보다 큰 경우
+            Debug.Log("굿엔딩: 히로인과 사귀게 되었다!");
+        }
+        else
+        {
+            //love가 70보다 작거나 같은 경우
+            Debug.Log("배드엔딩: 히로인에게 차였다.");
+        }
+
+        age = 11;
+
+        if (age>7 && age < 18)
+        {
+            Debug.Log("의무 교육을 받고 있습니다.");
+        }
+
+        if (age<13 || age > 70)
+        {
+            Debug.Log("일을 할 수 없는 나이입니다.");
+        }
+
+        /*for (int i = 0; i < 10; i++)
+        {
+            Debug.Log(i + "번째 순번입니다.");
+        }*/
+        
+        int i = 0;
+        while(i < 10)
+        {
+            Debug.Log(i + "번째 루프입니다.");
+            i++;
+        }
+        
+        int[] students = new int[5];
+        int score = 100;
+        for (int j = 0; j <5; j++)
+        {
+            students[j] = score;
+            score = score - 10;
+        }
+        for (int k = 0; k < students.Length; k++)
+        {
+            Debug.Log((k + 1) + "번 째 학생의 점수 : " + students[k]);
+        }
+    }
+    float GetDistance(float x1, float y1, float x2, float y2)
+    {
+        float width = x2 - x1;
+        float height = y2 - y1;
+
+        float distance=width*width+height*height;
+        distance = Mathf.Sqrt(distance);
+
+        return distance;
+    }
+}
